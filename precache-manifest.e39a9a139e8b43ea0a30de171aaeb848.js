6a90c603e8b0e08e2aa1880a638cfe6263529654
ISA002 <ggeasy1710@gmail.com>
2020-06-21 23:35:09 +0400

    Updates

---
+++ precache-manifest.e39a9a139e8b43ea0a30de171aaeb848.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1c3215c941a0f5a15ebe702c3b21dcb",
+    "url": "/TodoApp-with-React/index.html"
+  },
+  {
+    "revision": "b9b4e0670a558a21ab0b",
+    "url": "/TodoApp-with-React/static/css/main.3a58ad62.chunk.css"
+  },
+  {
+    "revision": "6e2f595f0e58ad8cfe3b",
+    "url": "/TodoApp-with-React/static/js/2.36982a41.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TodoApp-with-React/static/js/2.36982a41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9b4e0670a558a21ab0b",
+    "url": "/TodoApp-with-React/static/js/main.a821fcc0.chunk.js"
+  },
+  {
+    "revision": "6caca5b79abe8984c3b3",
+    "url": "/TodoApp-with-React/static/js/runtime-main.d01e3248.js"
+  },
+  {
+    "revision": "9341552d8cbfd150ac426cbd065c51a0",
+    "url": "/TodoApp-with-React/static/media/Merriweather-Light.9341552d.ttf"
+  }
+]);
